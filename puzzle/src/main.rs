7e5368af5a781517d81b25cfa3171008f427d6ec
yamakii <hiroshi_yamaki@caddi.jp>
2021-09-12 21:59:19 +0900

    moduleで分割

--- puzzle/src/main.rs
+++ puzzle/src/main.rs
@@ -1,75 +1,79 @@
 #![allow(dead_code)]
 
-use num::abs;
-use std::io;
-
 fn main() {
-    // triangle();
-    check_digit();
+    puzzle::triangle();
+    digit::check_digit();
 }
 
-fn triangle() {
-    for i in 0..7 {
-        for _ in 0..(4 - abs(4 - i)) {
-            print!("#");
+mod puzzle {
+    use num::abs;
+    pub fn triangle() {
+        for i in 0..7 {
+            for _ in 0..(4 - abs(4 - i)) {
+                print!("#");
+            }
+            println!();
         }
-        println!();
     }
 }
 
-fn check_digit() {
-    println!("Enter a numer");
-    let mut text = String::new();
-    io::stdin().read_line(&mut text).expect("error");
-    let text = text.trim();
+mod digit {
+    use std::io;
 
-    let (even, odd): (Vec<_>, Vec<_>) = text
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .enumerate()
-        .partition(|(i, _)| i % 2 == 0);
-    let odd_length_checksum = sum_double(&even) + sum_single(&odd);
-    let even_length_checksum = sum_single(&even) + sum_double(&odd);
+    pub fn check_digit() {
+        println!("Enter a numer");
+        let mut text = String::new();
+        io::stdin().read_line(&mut text).expect("error");
+        let text = text.trim();
 
-    let checksum = if text.chars().count() % 2 == 0 {
-        even_length_checksum
-    } else {
-        odd_length_checksum
-    };
+        let (even, odd): (Vec<_>, Vec<_>) = text
+            .chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .enumerate()
+            .partition(|(i, _)| i % 2 == 0);
+        let odd_length_checksum = sum_double(&even) + sum_single(&odd);
+        let even_length_checksum = sum_single(&even) + sum_double(&odd);
 
-    println!("Checksum is {}.", checksum);
-    if checksum % 10 == 0 {
-        println!("Checksum is divisilbe by 10. Valid.");
-    } else {
-        println!("Checksum is not divisilbe by 10. Invalid.");
+        let checksum = if text.chars().count() % 2 == 0 {
+            even_length_checksum
+        } else {
+            odd_length_checksum
+        };
+
+        println!("Checksum is {}.", checksum);
+        if checksum % 10 == 0 {
+            println!("Checksum is divisilbe by 10. Valid.");
+        } else {
+            println!("Checksum is not divisilbe by 10. Invalid.");
+        }
     }
-}
 
-fn sum_single(nums: &[(usize, u32)]) -> u32 {
-    nums.iter().map(|(_i, n)| n).sum::<u32>()
-}
-
-fn sum_double(nums: &[(usize, u32)]) -> u32 {
-    nums.iter()
-        .map(|(_i, n)| double_digit_number(*n))
-        .sum::<u32>()
-}
-
-fn double_digit_number(num: u32) -> u32 {
-    let result = num * 2;
-    if result < 10 {
-        result
-    } else {
-        result % 10 + 1
+    fn sum_single(nums: &[(usize, u32)]) -> u32 {
+        nums.iter().map(|(_i, n)| n).sum::<u32>()
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use crate::double_digit_number;
-    #[test]
-    fn test_double_digit_number() {
-        assert_eq!(double_digit_number(4), 8);
-        assert_eq!(double_digit_number(5), 1);
+    fn sum_double(nums: &[(usize, u32)]) -> u32 {
+        nums.iter()
+            .map(|(_i, n)| double_digit_number(*n))
+            .sum::<u32>()
+    }
+
+    fn double_digit_number(num: u32) -> u32 {
+        let result = num * 2;
+        if result < 10 {
+            result
+        } else {
+            result % 10 + 1
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::digit::double_digit_number;
+        #[test]
+        fn test_double_digit_number() {
+            assert_eq!(double_digit_number(4), 8);
+            assert_eq!(double_digit_number(5), 1);
+        }
     }
 }
