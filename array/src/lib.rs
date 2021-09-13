20c0a5d0a46174768d2924c1a4af86fe3efdfeb9
yamakii <hiroshi_yamaki@caddi.jp>
2021-09-13 22:02:11 +0900

    Optionを利用してロジックを整理

--- array/src/lib.rs
+++ array/src/lib.rs
@@ -2,31 +2,26 @@
 
 use std::{cmp::Ordering, collections::HashMap};
 
-fn find_mode(answers: &[u32]) -> usize {
+fn find_mode(answers: &[u32]) -> Option<u32> {
     let mut score = [0; 10];
     for answer in answers {
         if score.len() < *answer as usize {
-            return 0;
+            return None;
         }
         score[(*answer - 1) as usize] += 1;
     }
-    if let Some(index) = score
+    score
         .iter()
         .enumerate()
         .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap_or(Ordering::Equal))
-        .map(|(index, _)| index)
-    {
-        index + 1_usize
-    } else {
-        0
-    }
+        .map(|(index, _)| index as u32 + 1)
 }
 
-fn find_mode_by_hash_map(answers: &[u32]) -> u32 {
+fn find_mode_by_hash_map(answers: &[u32]) -> Option<u32> {
     let mut survay: HashMap<&u32, u32> = HashMap::new();
     for answer in answers {
         if *answer > 10 {
-            return 0;
+            return None;
         }
         if let Some(a) = survay.get_mut(answer) {
             *a += 1;
@@ -34,15 +29,10 @@ fn find_mode_by_hash_map(answers: &[u32]) -> u32 {
             survay.insert(answer, 1);
         }
     }
-    if let Some(answer) = survay
+    survay
         .iter()
         .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap_or(Ordering::Equal))
         .map(|(answer, _)| **answer)
-    {
-        answer
-    } else {
-        0
-    }
 }
 
 #[cfg(test)]
@@ -50,12 +40,12 @@ mod tests {
     use crate::*;
     #[test]
     fn test_find_mode() {
-        assert_eq!(10, find_mode(&[1, 10, 10, 1, 10]));
-        assert_eq!(0, find_mode(&[11]));
+        assert_eq!(Some(10), find_mode(&[1, 10, 10, 1, 10]));
+        assert_eq!(None, find_mode(&[11]));
     }
     #[test]
     fn test_find_mode_by_hash_map() {
-        assert_eq!(10, find_mode_by_hash_map(&[1, 10, 10, 1, 10]));
-        assert_eq!(0, find_mode_by_hash_map(&[11]));
+        assert_eq!(Some(10), find_mode_by_hash_map(&[1, 10, 10, 1, 10]));
+        assert_eq!(None, find_mode_by_hash_map(&[11]));
     }
 }
